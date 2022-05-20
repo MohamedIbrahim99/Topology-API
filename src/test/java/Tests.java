b1f47daed66363b841e5f666b23022ff7ab48350
Mohamed Ibrahim <mohamed.nino12@gmail.com>
2022-05-20 07:54:36 +0200

    tests update

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,6 +2,8 @@ import org.json.JSONException;
 import org.junit.Test;
 
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class Tests {
@@ -30,6 +32,14 @@ public class Tests {
     }
 
     @Test
+    public void queryTopologies_test2() {
+        String fileName = "src/main/resources/topology.json";
+        TopologyApi api = new TopologyApi();
+        api.readJSON(fileName);
+        assertEquals("top1", api.queryTopologies().get(0).getId());
+    }
+
+    @Test
     public void deleteTopology_test() {
         String fileName = "src/main/resources/topology.json";
         TopologyApi api = new TopologyApi();
@@ -47,11 +57,48 @@ public class Tests {
     }
 
     @Test
-    public void queryDevicesWithNetlistNode_test() {
+    public void queryDevices_test2() {
+        String fileName = "src/main/resources/topology.json";
+        TopologyApi api = new TopologyApi();
+        api.readJSON(fileName);
+        ArrayList<Component> devices = api.queryDevices("top1");
+        assertEquals("res1", devices.get(0).getId());
+    }
+
+    @Test
+    public void queryDevices_test3() {
+        String fileName = "src/main/resources/topology.json";
+        TopologyApi api = new TopologyApi();
+        api.readJSON(fileName);
+        ArrayList<Component> devices = api.queryDevices("top1");
+        assertEquals("m1", devices.get(1).getId());
+    }
+
+    @Test
+    public void queryDevicesWithNetlistNode_test1() {
         String fileName = "src/main/resources/topology.json";
         TopologyApi api = new TopologyApi();
         api.readJSON(fileName);// reading to get a topology
         // we can see in the file that only one node is in vdd nodelist
         assertEquals(1, api.queryDevicesWithNetlistNode("top1", "vdd").size());
     }
+
+    @Test
+    public void queryDevicesWithNetlistNode_test2() {
+        String fileName = "src/main/resources/topology.json";
+        TopologyApi api = new TopologyApi();
+        api.readJSON(fileName);// reading to get a topology
+        // we can see in the file that only one node is in vdd nodelist
+        assertEquals(2, api.queryDevicesWithNetlistNode("top1", "n1").size());
+    }
+
+    @Test
+    public void queryDevicesWithNetlistNode_test3() {
+        String fileName = "src/main/resources/topology.json";
+        TopologyApi api = new TopologyApi();
+        api.readJSON(fileName);// reading to get a topology
+        // we can see in the file that only one node is in vdd nodelist
+        assertEquals(1, api.queryDevicesWithNetlistNode("top1", "vss").size());
+    }
+
 }
